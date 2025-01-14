fc98616658beba33026c691cf3f91eccea0f0fec
Cosmin Coman <comancosmin61@outlook.com>
2025-01-14 13:13:36 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ComanCosminLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
